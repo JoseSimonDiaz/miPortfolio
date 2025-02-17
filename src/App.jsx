0f0dd257b162b37d0a95a5e3e09d522c58211fb4
JoseSimonDiaz <josesimondiaz13@gmail.com>
2025-02-17 02:40:17 -0300

    implemtento funcionalidad en el boton de wp

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,16 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import { motion } from 'framer-motion';
-import { FaWhatsapp } from 'react-icons/fa'; // Importar el ícono de WhatsApp
+import { FaWhatsapp } from 'react-icons/fa'; 
 
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  
+  const phoneNumber = '+5493816179958'; 
+  const defaultMessage = 'Hola José, estoy interesado en tus servicios.'; 
+  const encodedMessage = encodeURIComponent(defaultMessage);
+  const whatsappLink = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
 
   return (
     <motion.div 
@@ -21,15 +27,15 @@ function App() {
       <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <div className={`transition-all duration-300 ${isMenuOpen ? 'filter blur-sm' : ''}`}>
         <Hero />
-        <Projects />
         <AboutSection />
         <Skills />
+        <Projects />
         <Contact />
       </div>
 
-      {/* Ícono de WhatsApp */}
+    
       <a
-        href="https://wa.me/+5493816179958" // Reemplaza con tu número de WhatsApp
+        href={whatsappLink} 
         target="_blank"
         rel="noopener noreferrer"
         className="fixed bottom-8 right-8 z-50"
